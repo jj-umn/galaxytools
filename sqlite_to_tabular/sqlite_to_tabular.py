ff0130c653f6240d35a7c08dd37c767b0228e7df
Jim Johnson <jj@umn.edu>
2015-10-04 09:45:16 -0500

    Pass query to script in a file.  Four char indents.

--- sqlite_to_tabular/sqlite_to_tabular.py
+++ sqlite_to_tabular/sqlite_to_tabular.py
@@ -10,6 +10,7 @@ def __main__():
   parser = optparse.OptionParser()
   parser.add_option( '-s', '--sqlitedb', dest='sqlitedb', default=None, help='The SQLite Database' )
   parser.add_option( '-q', '--query', dest='query', default=None, help='SQL query' )
+  parser.add_option( '-Q', '--query_file', dest='query_file', default=None, help='SQL query file' )
   parser.add_option( '-n', '--no_header', dest='no_header', action='store_true', default=False, help='Include a column headers line' )
   parser.add_option( '-o', '--output', dest='output', default=None, help='Output file for query results' )
   (options, args) = parser.parse_args()
@@ -25,7 +26,16 @@ def __main__():
   else:
     outputFile = sys.stdout
 
-  if (options.query is None):
+  query = None
+  if (options.query_file != None):
+    with open(options.query_file,'r') as fh:
+      query = ''
+      for line in fh:
+        query += line
+  elif (options.query != None):
+    query = options.query
+
+  if (query is None):
     try:
       conn = sqlite.connect(options.sqlitedb)
       c = conn.cursor()
@@ -42,14 +52,14 @@ def __main__():
           print >> sys.stderr, "Error: %s" % exc
     except Exception, exc:
       print >> sys.stderr, "Error: %s" % exc
-    exit(1)
-  #if not sqlite.is_read_only_query(options.query):
+    exit(0)
+  #if not sqlite.is_read_only_query(query):
   #  print >> sys.stderr, "Error: Must be a read only query"
   #  exit(2)
   try:
     conn = sqlite.connect(options.sqlitedb)
     cur = conn.cursor()
-    results = cur.execute(options.query)
+    results = cur.execute(query)
     if not options.no_header:
       outputFile.write("#%s\n" % '\t'.join([str(col[0]) for col in cur.description]))
         # yield [col[0] for col in cur.description]
